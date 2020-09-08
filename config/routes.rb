29acc9416e3cd8df2f460e7da607a9bf18b28ed4
EC2 Default User <ec2-user@ip-172-31-76-156.ec2.internal>
2020-09-08 12:13:08 +0000

    [Add] コメント投稿機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show]
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
